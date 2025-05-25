30c119925ea5d7d2101243c82fb63d38d442eb6f
hyx19980703 <401543785@qq.com>
2025-05-25 15:24:12 +0800

    解决冲突2

--- Assets/Script/CheckPoint.cs
+++ Assets/Script/CheckPoint.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-<<<<<<< HEAD
+
     private bool isActive;
     [SerializeField] private int currenLevel; //该存档点对应的关卡
-=======
-    private bool isActive;  //激活检查点
+
     [SerializeField] private Sprite activePoint;
     private SpriteRenderer spriteRenderer;
->>>>>>> leveldesign
+
 
     void Awake()
     {
@@ -22,11 +21,9 @@ public class CheckPoint : MonoBehaviour
     {
         if (isActive == false && collision.CompareTag("Player"))
         {
-<<<<<<< HEAD
+
             GameManager.Instance.SetCheckPoint(transform.position,currenLevel);
-=======
-            GameManager.Instance.SetCheckPoint(transform.position);//设置最后检查点
->>>>>>> leveldesign
+
             isActive = true;
             spriteRenderer.sprite = activePoint;
             
